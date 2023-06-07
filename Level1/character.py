651e34cbae08114ea8be4f149362443974559ab0
TechDanny <kokidanny15@gmail.com>
2023-06-07 07:07:45 +0000

    print characters

---
+++ Level1/character.py
@@ -0,0 +1,8 @@
+"""Write a program that takes a string as input and
+prints each character on a separate line using a for loop
+"""
+
+
+name = input("Type a string: ")
+for char in name:
+    print(char)
