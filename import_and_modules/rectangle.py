6b5014ba8030e6faec416071f93e6d81ef076aea
TechDanny <kokidanny15@gmail.com>
2023-06-10 21:43:44 +0000

    perimeter and area of rectangle

---
+++ import_and_modules/rectangle.py
@@ -0,0 +1,15 @@
+"""
+Create a Python Program that calculates both the
+perimeter and area of a rectangle
+"""
+
+
+from func_rectangle import Rectangle, Rectangle_area
+
+
+length = int(input("Length: "))
+width = int(input("Width: "))
+perim = Rectangle(length, width)
+area = Rectangle_area(length, width)
+print(f"Perimeter of rectangle: {perim}")
+print(f"Area of rectangle: {area}")
