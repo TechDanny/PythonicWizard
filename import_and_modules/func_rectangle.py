9b3ff0a7669a6741ec4c50f1ca01338f7845f53a
TechDanny <kokidanny15@gmail.com>
2023-06-10 21:45:11 +0000

    helping function to find perimeter and area

---
+++ import_and_modules/func_rectangle.py
@@ -0,0 +1,8 @@
+def Rectangle(length, width):
+    perimeter = length + width + length + width
+    return perimeter
+
+
+def Rectangle_area(length, width):
+    area = length * width
+    return area
