1dd2495d68c4040c02d086eb1aa24ea89a4ad611
TechDanny <kokidanny15@gmail.com>
2023-06-16 19:57:56 +0000

    calculates area of a circle

--- Level1/circle.py
+++ Level1/circle.py
@@ -1,6 +1,6 @@
 from pi import formular
 
 
-radius = int(input("What is the radius? "))
+radius = int(input("Radius of circle: "))
 area_of_circle = formular(radius)
 print(f"Area of a circle is: {area_of_circle}")
